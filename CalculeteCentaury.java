d40f4a8f0476ca3afcbe2180470de11e48c55c54
xzunun <jmg201098@gmail.com>
2023-01-02 15:09:01 -0600

    Class and method name modified

--- CalculeteCentaury.java
+++ CalculeteCentaury.java
@@ -1,10 +1,10 @@
-public class CalcularSiglo {
+public class CalculateCentaury {
 	public static int centaury = 100; // Se crea una field llamada "centaury" y se inicializa en 100 que es, lo que equivale un siglo.
 	public static int countCentaury = 1; // Se crea una field llamada "countCentaury" se inicializa en 1, ya que de ahí parte un siglo.
 	public static int savedCentaury = 0; // Se crea una field llamada "savedCentaury" se inicializa en 0, donde se guardará el siglo que se encuentre con el año.
 	
 	// Se crea un método que retorna un valor entero (int) llamda "calcularSiglo" como parametros se requiere una variable de tipo entero, que es el año.
-	public static int calcularSiglo(int year) {
+	public static int centaury(int year) {
 		for(int i = 1; i < 4000; i++) {
 			//System.out.println(i);
 			if(i == (centaury + 1)) {
@@ -33,6 +33,6 @@ public class CalcularSiglo {
 	}
 	public static void main(String[] args) {
 		int year = 1;
-		System.out.println(year + " pertenece al siglo " + calcularSiglo(year));
+		System.out.println(year + " pertenece al siglo " + centaury(year));
 	}
 }
